e332ee2cd432127b071af624e5e3359277231e27
Kevin Escorcia <165961257+KevinLise@users.noreply.github.com>
2026-04-27 09:59:44 -0500

    Add files via upload

--- Prueba_Kevin/script.js
+++ Prueba_Kevin/script.js
@@ -1,16 +1,16 @@
-// Park menu
-const park = document.getElementById('park');
+// Hamburger menu
+const parkt = document.getElementById('parkt');
 const nav = document.getElementById('main-nav');
 
-    park.addEventListener('click', () => {
-    park.classList.toggle('open');
+parkt.addEventListener('click', () => {
+    parkt.classList.toggle('open');
     nav.classList.toggle('open');
 });
 
 // Cierra el menú al hacer clic en un enlace
-nav.querySelectorAll('a').forEach(link => {
+    nav.querySelectorAll('a').forEach(link => {
     link.addEventListener('click', () => {
-        park.classList.remove('open');
-        nav.classList.remove('open');
+    parkt.classList.remove('open');
+    nav.classList.remove('open');
     });
 });
